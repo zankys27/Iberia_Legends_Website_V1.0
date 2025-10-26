cfbe08038f9a7eb92e163dd4b3be1bd12b24b05e
zankys27 <pcpi.adrian.garcia@gmail.com>
2025-10-26 11:04:24 +0100

    Add files via upload
    update7.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import IberiaLegendsDemo from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+    <IberiaLegendsDemo />
+  </React.StrictMode>,
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
